cf9092a8652f5afbf3ec01e207197fa517722859
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-02 21:27:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten() {
-  kittens.append("lucy");
+  kittens.push("lucy");
   return kittens;
 }
 console.log(kittens);
